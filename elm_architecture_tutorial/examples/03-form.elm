40af8dc466b80901c5831fd65a8fc43af9b9a7b2
Bairstow <joshbairstow@gmail.com>
2017-12-25 13:23:08 +1100

    Add elm architecture tute files

---
+++ elm_architecture_tutorial/examples/03-form.elm
@@ -0,0 +1,90 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import Char exposing (isUpper, isLower, isDigit)
+
+
+main =
+  Html.beginnerProgram
+    { model = model
+    , view = view
+    , update = update
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { name : String
+  , password : String
+  , passwordAgain : String
+  }
+
+
+model : Model
+model =
+  Model "" "" ""
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Name String
+    | Password String
+    | PasswordAgain String
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Name name ->
+      { model | name = name }
+
+    Password password ->
+      { model | password = password }
+
+    PasswordAgain password ->
+      { model | passwordAgain = password }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ type_ "text", placeholder "Name", onInput Name ] []
+    , input [ type_ "password", placeholder "Password", onInput Password ] []
+    , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+    , viewValidation model
+    ]
+
+
+viewValidation : Model -> Html msg
+viewValidation model =
+  let
+    (color, message) =
+      if String.length model.password <= 8 then
+        ("red", "Password must be longer than 8 characters")
+
+      else if String.any isUpper model.password == False then
+        ("red", "Password must contain an uppercase character")
+
+      else if String.any isLower model.password == False then
+        ("red", "Password must contain a lowercase character")
+
+      else if String.any isDigit model.password == False then
+        ("red", "Password must contain a numeric character")
+
+      else if model.password /= model.passwordAgain then
+        ("red", "Passwords do not match!")
+
+      else
+        ("green", "OK")
+  in
+    div [ style [("color", color)] ] [ text message ]
