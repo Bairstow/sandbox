7fe1f15f4ab5e1488f9ca54639edbec71a50501c
Bairstow <joshbairstow@gmail.com>
2018-12-27 01:52:49 +1100

    Add empty creep object check

--- sc-main/src/memory.js
+++ sc-main/src/memory.js
@@ -1,6 +1,7 @@
 const manager = () => {
+  if (!Memory.creeps) return;
   const creepNames = Object.keys(Memory.creeps);
-  creepNames.forEach((name) => {
+  creepNames.forEach(name => {
     if (!Game.creeps[name]) {
       delete Memory.creeps[name];
       console.log(`Clearing non-existing creep memory: ${name}`);
@@ -9,5 +10,5 @@ const manager = () => {
 };
 
 module.exports = {
-  manager
+  manager,
 };
